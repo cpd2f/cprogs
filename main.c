c5f8268d616441d6b9d7b73de86290967bc9d02c
Kaito Luijbregts <kluijbregtsk@gmail.com>
2025-06-01 21:13:04 +0200

    Hello World program

---
+++ main.c
@@ -0,0 +1,12 @@
+// includes the standard input-output header
+#include <stdio.h>
+
+// entry point for C and C++ programs
+int main() {
+
+  // prints a string of characters into the screen, in this case "Hello world!"
+  printf("Hello world!\n")
+
+  // terminates a program successfully
+  return 0;
+}
